0d5547c83717bec4a0d1ba0d71b003a8230bc2c4
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2024-05-16 18:48:58 +0300

    Add ConversationHandler

--- bot.py
+++ bot.py
@@ -2,58 +2,93 @@ import logging
 import random
 import redis
 import json
+import re
 
 from functools import partial
 from environs import Env
-from telegram import Update
-from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, CallbackContext
+from telegram import Update, ReplyKeyboardRemove
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, CallbackContext, ConversationHandler
 
 from keyboards import get_main_keyboard
 from get_quiz_content import load_quiz_content
 
 
-def start(update: Update, context: CallbackContext):
+def start(update: Update, context: CallbackContext, db):
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text="Здравствуйте!",
                              reply_markup=get_main_keyboard(),
                              )
 
+    return 'REQUEST_QUESTION'
 
-def echo(update: Update, context: CallbackContext, db):
-    if update.message.text == 'Новый вопрос':
-        quiz_content = load_quiz_content()
-        question = random.choice(quiz_content)
-        question_json = json.dumps(question)
-        db.set(str(update.effective_chat.id), question_json)
+
+def handle_solution_attempt(update: Update, context: CallbackContext, db):
+    question = json.loads(db.get(str(update.effective_chat.id)))
+    answer = re.split(r'[.(]', question[1])[0].lower()
+    logger.info(answer)
+    logger.info(question[1])
+
+    if answer == update.message.text.lower():
+        text = 'Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»'
         context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text=question[0],
+                                 text=text,
                                  reply_markup=get_main_keyboard(),
                                  )
+
+        return 'REQUEST_QUESTION'
+
     else:
-        question = json.loads(db.get(str(update.effective_chat.id)))
+        text='Неправильно… Попробуешь ещё раз?'
         context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text=question[1],
+                                 text=text,
                                  reply_markup=get_main_keyboard(),
-                                )
+                                 )
+
+        return 'WAITING_ANSWER'
 
 
-def new_question(update: Update, context: CallbackContext):
+def handle_new_question_request(update: Update, context: CallbackContext, db):
     quiz_content = load_quiz_content()
-
+    question = random.choice(quiz_content)
+    question_json = json.dumps(question)
+    db.set(str(update.effective_chat.id), question_json)
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text='Ваш вопрос принят. Мы ответим в ближайшее время.',
+                             text=question[0],
                              reply_markup=get_main_keyboard(),
-                            )
+                             )
+
+    return 'WAITING_ANSWER'
+
+
+def cancel(update: Update, context: CallbackContext):
+    update.message.reply_text(text='Пока!',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
+
 
 def start_tg_bot(token, logger, db):
     updater = Updater(token=token, use_context=True)
     dispatcher = updater.dispatcher
 
-    start_handler = CommandHandler('start', start)
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), partial(echo, db=db))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', partial(start, db=db))],
 
-    dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(echo_handler)
+        states={
+            'WAITING_ANSWER': [
+                MessageHandler(Filters.text & (~Filters.command),
+                               partial(handle_solution_attempt,db=db))
+            ],
+            'REQUEST_QUESTION': [
+                MessageHandler(Filters.text & Filters.regex(r'^Новый вопрос$'),
+                               partial(handle_new_question_request, db=db)),
+            ],
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+
+    dispatcher.add_handler(conv_handler)
 
     logger.info('Start telegram bot')
     updater.start_polling()
